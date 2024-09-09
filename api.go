26378821bf7016b0d9c3f6d102b89c1922f424f8
Dylan Lewis <Dylan17525@gmail.com>
2024-09-09 21:18:47 +0300

    commit 1

--- api.go
+++ api.go
@@ -9,9 +9,56 @@ import (
 	"github.com/gorilla/mux"
 )
 
+func (s *APIServer) Run() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/account", makeHTTPHandlerFunc(s.HandleAccount))
+
+	router.HandleFunc("/account/{id}", makeHTTPHandlerFunc(s.HandleGetAccount))
+
+	log.Println("Starting server on", s.listenAddr)
+
+	http.ListenAndServe(s.listenAddr, router)
+}
+
+func (s *APIServer) HandleAccount(w http.ResponseWriter, r *http.Request) error {
+	if r.Method == http.MethodGet {
+		return s.HandleGetAccount(w, r)
+	}
+	if r.Method == http.MethodPost {
+		return s.HandleCreateAccount(w, r)
+	}
+	if r.Method == http.MethodDelete {
+		return s.HandleDeleteAccount(w, r)
+	}
+	if r.Method == http.MethodPut {
+		return s.HandleTransfer(w, r)
+	}
+	return fmt.Errorf("unsupported method %s", r.Method)
+}
+
+func (s *APIServer) HandleGetAccount(w http.ResponseWriter, r *http.Request) error {
+	id := mux.Vars(r)["id"]
+
+	fmt.Println("Getting account", id)
+
+	return WriteJSON(w, http.StatusOK, &account{})
+}
+
+func (s *APIServer) HandleCreateAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+func (s *APIServer) HandleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
+func (s *APIServer) HandleTransfer(w http.ResponseWriter, r *http.Request) error {
+	return nil
+}
+
 func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
-	w.Header().Set("Content-Type", "application/json")
 	return json.NewEncoder(w).Encode(v)
 }
 
@@ -38,45 +85,4 @@ func NewAPIServer(listenAddr string) *APIServer {
 	return &APIServer{
 		listenAddr: listenAddr,
 	}
-}
-
-func (s *APIServer) Run() {
-	router := mux.NewRouter()
-
-	router.HandleFunc("/account", makeHTTPHandlerFunc(s.HandleAccount))
-
-	log.Println("Starting server on", s.listenAddr)
-
-	http.ListenAndServe(s.listenAddr, router)
-}
-
-func (s *APIServer) HandleAccount(w http.ResponseWriter, r *http.Request) error {
-	if r.Method == http.MethodGet {
-		return s.HandleGetAccount(w, r)
-	}
-	if r.Method == http.MethodPost {
-		return s.HandleCreateAccount(w, r)
-	}
-	if r.Method == http.MethodDelete {
-		return s.HandleDeleteAccount(w, r)
-	}
-	if r.Method == http.MethodPut {
-		return s.HandleTransfer(w, r)
-	}
-	return fmt.Errorf("unsupported method %s", r.Method)
-}
-
-func (s *APIServer) HandleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
-}
-
-func (s *APIServer) HandleCreateAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
-}
-func (s *APIServer) HandleDeleteAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
-}
-
-func (s *APIServer) HandleTransfer(w http.ResponseWriter, r *http.Request) error {
-	return nil
 }
